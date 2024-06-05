e6d6f83dfedbc63767e592e319683ddadf2cee96
t.saito-H13 <sct202312miyagi.tomohiro.saitou@gmail.com>
2024-06-05 15:06:22 +0900

    no message

--- main/resources/static/js/modal.js
+++ main/resources/static/js/modal.js
@@ -1,9 +1,12 @@
-//deletelist.htmlの削除ボタンクリック時発火するモーダル
-$(document).ready(function() {
-    $('a.btn-secondary').on('click', function(e) {
-        e.preventDefault();
-        var href = $(this).attr('href');
-        $('#deleteBtn').attr('href', href);
-        $('#confirmModal').modal();
-    });
-});
+var myModal = document.getElementById('confirmModal')
+myModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget
+
+  // Extract info from data-bs-* attributes
+  var userId = button.getAttribute('data-bs-user-id')
+
+  // Update the modal's content.
+  var deleteBtn = myModal.querySelector('#deleteBtn')
+  deleteBtn.href = "/user/" + userId + "/delete"
+})
